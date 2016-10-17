ecd83040351131dcaac3eeb262daedafef756bf8
Marcos Cifuentes <marcosnaza16@gmail.com>
2016-10-16 23:21:54 -0300

    sql DB

--- sql/cinema_false.sql
+++ sql/cinema_false.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.0.10deb1
+-- version 4.3.11
 -- http://www.phpmyadmin.net
 --
--- Servidor: localhost
--- Tiempo de generación: 14-10-2016 a las 12:09:24
--- Versión del servidor: 5.5.49-0ubuntu0.14.04.1
--- Versión de PHP: 5.5.9-1ubuntu4.17
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 17-10-2016 a las 04:21:01
+-- Versión del servidor: 5.6.24
+-- Versión de PHP: 5.6.8
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -27,11 +27,11 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE IF NOT EXISTS `contacto` (
-  `id_contacto` int(11) NOT NULL AUTO_INCREMENT,
+  `id_contacto` int(11) NOT NULL,
   `nombre y apellido` varchar(50) NOT NULL,
-  `mensaje` varchar(200) NOT NULL,
-  PRIMARY KEY (`id_contacto`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+  `email` varchar(50) NOT NULL,
+  `mensaje` varchar(200) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -40,10 +40,9 @@ CREATE TABLE IF NOT EXISTS `contacto` (
 --
 
 CREATE TABLE IF NOT EXISTS `genero` (
-  `id_genero` int(11) NOT NULL AUTO_INCREMENT,
-  `genero` varchar(20) NOT NULL,
-  PRIMARY KEY (`id_genero`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+  `id_genero` int(11) NOT NULL,
+  `genero` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -52,12 +51,11 @@ CREATE TABLE IF NOT EXISTS `genero` (
 --
 
 CREATE TABLE IF NOT EXISTS `horario` (
-  `id_horario` int(11) NOT NULL AUTO_INCREMENT,
+  `id_horario` int(11) NOT NULL,
   `fk_pelicula` int(11) NOT NULL,
   `sala` int(11) NOT NULL,
-  `horario` time NOT NULL,
-  PRIMARY KEY (`id_horario`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+  `horario` time NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -66,15 +64,66 @@ CREATE TABLE IF NOT EXISTS `horario` (
 --
 
 CREATE TABLE IF NOT EXISTS `pelicula` (
-  `id_pelicula` int(11) NOT NULL AUTO_INCREMENT,
+  `id_pelicula` int(11) NOT NULL,
   `titulo` varchar(50) NOT NULL,
   `descripcion` varchar(500) NOT NULL,
   `duracion` int(11) NOT NULL,
   `fk_genero` int(11) NOT NULL,
-  `imagen` varchar(100) NOT NULL,
-  PRIMARY KEY (`id_pelicula`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+  `imagen` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `contacto`
+--
+ALTER TABLE `contacto`
+  ADD PRIMARY KEY (`id_contacto`);
+
+--
+-- Indices de la tabla `genero`
+--
+ALTER TABLE `genero`
+  ADD PRIMARY KEY (`id_genero`);
+
+--
+-- Indices de la tabla `horario`
+--
+ALTER TABLE `horario`
+  ADD PRIMARY KEY (`id_horario`);
+
+--
+-- Indices de la tabla `pelicula`
+--
+ALTER TABLE `pelicula`
+  ADD PRIMARY KEY (`id_pelicula`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `contacto`
+--
+ALTER TABLE `contacto`
+  MODIFY `id_contacto` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT de la tabla `genero`
+--
+ALTER TABLE `genero`
+  MODIFY `id_genero` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT de la tabla `horario`
+--
+ALTER TABLE `horario`
+  MODIFY `id_horario` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT de la tabla `pelicula`
+--
+ALTER TABLE `pelicula`
+  MODIFY `id_pelicula` int(11) NOT NULL AUTO_INCREMENT;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
